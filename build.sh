80c4f8e2a98c54ff562864f7a7e8d1b37564612c
Itwerntme <72857681+Itwernme@users.noreply.github.com>
2024-02-12 22:38:37 +0000

    git ignore

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 cc -g -std=c99 -c main.c -o obj/main.o
 cc -g -std=c99 -c game.c -o obj/game.o
-cc -o main obj/main.o obj/game.o -s -Wall -std=c99 -lraylib -lm -lpthread -ldl -lrt
-./main
+cc -o build/main obj/main.o obj/game.o -s -Wall -std=c99 -lraylib -lm -lpthread -ldl -lrt
+./build/main
